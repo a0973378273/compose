0eab12e2b4d460de79ba9ce45a4ee02f44967d7c
DESKTOP-S2HLGBV\bean <bean>
2023-03-25 18:15:16 +0800

    Add constrainLayout

--- app/src/main/java/bean/sample/compose/MainActivity.kt
+++ app/src/main/java/bean/sample/compose/MainActivity.kt
@@ -3,17 +3,22 @@ package bean.sample.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import bean.sample.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingsList()
                 }
             }
         }
@@ -34,28 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun animation(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-    Row {
-        Text(text = "Row1 $name!")
-        Text(text = "Row2 $name!")
-        Column(Modifier.padding(top = extraPadding)) {
-            Text(text = "Column1 $name!")
-            Text(if (expanded.value) "Show less" else "Show more")
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value },
-            ) {
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 200.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessVeryLow
+        )
+    )
+    ConstraintLayout{
+        val (row1,row2) = createRefs()
+        Row {
+            Text(text = "Row1 $name!")
+            Text(text = "Row2 $name!")
+            Column(Modifier.padding(top = extraPadding.coerceAtLeast(10.dp))) {
+                Text(text = "Column1 $name!")
                 Text(if (expanded.value) "Show less" else "Show more")
+                ElevatedButton(
+                    onClick = { expanded.value = !expanded.value },
+                ) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
+            Row {
+                Text(text = "Row3 $name!")
+            }
+            Box(modifier = Modifier.background(Color.Red).fillMaxWidth(),contentAlignment = Alignment.Center) {
+                Text(text = "Box1")
             }
         }
-    }
-    Row {
-        Text(text = "Row3 $name!")
-//        Box {
-//            Text(text = "Box1 $name!")
-//            Text(text = "Box2 $name!")
-//        }
     }
 }
 
@@ -78,7 +91,6 @@ fun OnboardingScreen(
     onContinueClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -101,7 +113,18 @@ private fun Greetings(
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Greeting(name = name)
+            animation(name = name)
+        }
+    }
+}
+
+@Composable
+private fun GreetingsList(
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn {
+        items(items = names) { name ->
+            animation(name = name)
         }
     }
 }
@@ -110,6 +133,6 @@ private fun Greetings(
 @Composable
 fun DefaultPreview() {
     ComposeTheme {
-        Greeting("1")
+        GreetingsList()
     }
 }
